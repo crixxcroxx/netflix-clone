4d0e0ae4100627bdb489c43e2e928a4ac800f1e3
crixxcroxx <cristajesawa@gmail.com>
2021-12-30 13:50:20 +0800

    Add Footer Component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import "./footer.scss";
+
+export default function Footer() {
+
+  return (
+    <footer>
+      <div className="f">
+        <p className="title">Questions? Contact us.</p>
+        <div>
+          <p>FAQ</p>
+          <p>Investor Relations</p>
+          <p>Ways to Watch</p>
+          <p>Corporate Information</p>
+          <p>Only on Netflix</p>
+        </div>
+        <p>Netflix Philippines</p>
+      </div>
+
+      <div>
+        <p>Help Center</p>
+        <p>Jobs</p>
+        <p>Terms of Use</p>
+        <p>Contact Us</p>
+      </div>
+
+      <div>
+        <p>Account</p>
+        <p>Redeem Gift Cards</p>
+        <p>Privacy</p>
+        <p>Speed Test</p>
+      </div>
+
+      <div>
+        <p>Media Center</p>
+        <p>Buy Gift Cards</p>
+        <p>Cookie Preferences</p>
+        <p>Legal Notices</p>
+      </div>
+    </footer>
+  );
+}
